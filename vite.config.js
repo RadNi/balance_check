a6f16dec43b1fc961009e6f9d376017122ed38e5
amirhossein <khajepour.amirhossein@gmail.com>
2025-06-29 15:31:17 -0700

    fixed buffer issue. upgraded to version 1.0.0 but prover stucks

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,40 @@
-export default {
+import { defineConfig } from 'vite'
+
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+export default defineConfig({
+  plugins: [
+    nodePolyfills({
+      include: ['path', 'stream', 'util'],
+      exclude: ['http'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      overrides: {
+        fs: 'memfs',
+      },
+      protocolImports: true,
+    }),
+  ],
+  // other options
+  // resolve: {
+  //   alias: {
+  //     buffer: 'buffer/',
+  //   }
+  // },
+
   optimizeDeps: {
     esbuildOptions: { target: "esnext" },
     exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js']
   }
-};
+})
+
+// export default {
+// };
+
+// export default defineConfig({
+//   // other options
+//   plugins: [nodePolyfills()]
+// })
